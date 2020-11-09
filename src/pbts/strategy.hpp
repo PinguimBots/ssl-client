f640e6013b6709e02345e929ade3cc53adef8426
Gilberto Kreisler <gkreisler5@hotmail.com>
2020-11-09 18:36:05 -0300

    feat: many things

--- src/pbts/strategy.hpp
+++ src/pbts/strategy.hpp
@@ -33,6 +33,27 @@ namespace pbts
             )
             -> std::vector<pbts::point>;
 
+        auto create_path(
+            const pbts::point goal_position,
+            const pbts::robot &allied_robot,
+            const std::vector<pbts::point> &enemy_robots)
+            -> pbts::point;
+        
+        auto actions(
+            const pbts::field_geometry& field,
+            const pbts::robot& robot,
+            const pbts::ball& ball,
+            const std::vector<pbts::point> &enemy_robots,
+            double team
+        ) -> std::tuple<pbts::point, int>;
+
+        auto setTeam(bool isYellow) -> void;
+
+        auto setBounds(pbts::field_geometry bounds) -> void;
+
+        auto kick(const pbts::robot& robot, const pbts::ball& ball) -> std::tuple<pbts::point, int>;
+
+    private:
         static const constexpr auto xmin = -1.0;
         static const constexpr auto xmax = +1.0;
         static const constexpr auto xT = xmax - xmin; //m
@@ -50,39 +71,40 @@ namespace pbts
         static const constexpr int imax = std::round(xT/dx) + imin;
         static const constexpr int jmax = std::round(yT/dy) + jmin;
 
-        auto create_path(
-            const pbts::point goal_position,
-            const pbts::robot &allied_robot,
-            const std::vector<pbts::point> &enemy_robots)
-            -> pbts::point;
-        
-        auto actions(
-            const pbts::field_geometry& field,
-            const pbts::robot& robot,
-            const pbts::ball& ball,
-            const std::vector<pbts::point> &enemy_robots,
-            double team
-        ) -> std::tuple<pbts::point, int>;
+        pbts::field_geometry field_bounds;
 
-        auto kick(const pbts::robot& robot, const pbts::ball& ball) -> std::tuple<pbts::point, int>;
+        bool is_yellow;
 
-    private:
+        //Só pros scanf;
+        //char c;
+
         bool isNear(pbts::point point1, pbts::point point2, double tol);
+        
+        // Verificação de vizinhança
         auto four_neighborhood(pbts::wpoint point, int radius) -> std::vector<pbts::wpoint>;
         auto d_neighborhood(pbts::wpoint point, int radius) -> std::vector<pbts::wpoint>;
         auto valid_neighbours(pbts::wpoint point, int ntype, int radius) -> std::vector<pbts::wpoint>;
+        
+        //Wave planner
         auto wave_planner(
             const pbts::wpoint goal_position,
             const pbts::wpoint allied_robot,
             const std::vector<pbts::wpoint> &enemy_robots)
             -> pbts::wpoint;
+
+        auto wave_path(int (&field)[imax][jmax], const pbts::wpoint goal, std::vector<std::vector<int>> &cost) -> void;
+        
+        // Populando a matriz
         auto generate_obstacle(int (&field)[imax][jmax], const std::vector<pbts::wpoint> &enemy_robots) -> void;
-        auto next_point(const pbts::wpoint pos_now, std::vector<std::vector<int>> &cost) -> pbts::wpoint;
-        auto add_clearance(int (&field)[imax][jmax], const pbts::wpoint robot_position) -> void;
-        auto wave_path(int (&field)[imax][jmax], const pbts::wpoint goal) -> std::vector<std::vector<int>>;
+        auto next_point(const pbts::wpoint pos_now, const pbts::wpoint goal, std::vector<std::vector<int>> &cost) -> pbts::wpoint;
+        auto add_clearance(int (&field)[imax][jmax], const pbts::wpoint goal_position) -> void;
+        auto add_shield_ball(int (&field)[imax][jmax], const pbts::wpoint ball) -> void;
+        
         auto pertoBola(std::vector<pbts::robot>& allied_robots, const std::vector<pbts::robot> &enemy_robots,  const pbts::ball& ball) -> int;
+        
         auto discreet_to_real(pbts::wpoint wpoint) -> pbts::point;
         auto real_to_discreet(pbts::point point) -> pbts::wpoint;
+
         auto rotate(const pbts::robot& robot, const pbts::ball& ball) -> std::tuple<pbts::point, int>;
        
         auto trackBallYAxix(const pbts::robot& rbt, const pbts::ball& ball, double team) -> std::tuple<pbts::point, int>;
