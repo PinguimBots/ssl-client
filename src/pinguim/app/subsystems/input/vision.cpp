1b3ffa08fafd20cd7d59d391c497a6bd4f96e1c1
fbdss <fbdsschaun@inf.ufpel.edu.br>
2022-02-27 20:33:32 -0400

    chore: rename *.*.inc files to *.inc.*

--- src/pinguim/app/subsystems/input/vision.cpp
+++ src/pinguim/app/subsystems/input/vision.cpp
@@ -1,7 +1,7 @@
 #include "pinguim/conf.hpp"
 
 #if defined(PINGUIM_CONF_OPENCV_SUPPORT)
-#include "pinguim/app/subsystems/input/vision_enabled.cpp.inc"
+#include "pinguim/app/subsystems/input/vision_enabled.inc.cpp"
 #else
-#include "pinguim/app/subsystems/input/vision_disabled.cpp.inc"
+#include "pinguim/app/subsystems/input/vision_disabled.inc.cpp"
 #endif
--- src/pinguim/app/subsystems/input/vision.hpp
+++ src/pinguim/app/subsystems/input/vision.hpp
@@ -3,7 +3,7 @@
 #include "pinguim/conf.hpp"
 
 #if defined(PINGUIM_CONF_OPENCV_SUPPORT)
-#include "pinguim/app/subsystems/input/vision_enabled.hpp.inc"
+#include "pinguim/app/subsystems/input/vision_enabled.inc.hpp"
 #else
-#include "pinguim/app/subsystems/input/vision_disabled.hpp.inc"
+#include "pinguim/app/subsystems/input/vision_disabled.inc.hpp"
 #endif
