d747015c80895508d67ec174e25d1bae6ae4122a
fbdss <fbdsschaun@inf.ufpel.edu.br>
2022-03-02 15:30:35 -0400

    chore: add user-defined-literals for our aliases

--- src/pinguim/aliases.hpp
+++ src/pinguim/aliases.hpp
@@ -15,6 +15,19 @@ namespace pinguim::inline integer_aliases
     using i16 = std::int16_t;
     using i32 = std::int32_t;
     using i64 = std::int64_t;
+
+    inline namespace literals
+    {
+        constexpr auto operator""_u8(unsigned long long val)  { return static_cast<u8>(val); }
+        constexpr auto operator""_u16(unsigned long long val) { return static_cast<u16>(val); }
+        constexpr auto operator""_u32(unsigned long long val) { return static_cast<u32>(val); }
+        constexpr auto operator""_u64(unsigned long long val) { return static_cast<u64>(val); }
+
+        constexpr auto operator""_i8(unsigned long long val)  { return static_cast<i8>(val); }
+        constexpr auto operator""_i16(unsigned long long val) { return static_cast<i16>(val); }
+        constexpr auto operator""_i32(unsigned long long val) { return static_cast<i32>(val); }
+        constexpr auto operator""_i64(unsigned long long val) { return static_cast<i64>(val); }
+    };
 }
 
 namespace pinguim::inline float_aliases
