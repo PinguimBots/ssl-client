b36485d82391b37341e27bf40db7207f36dc66e9
fbdss <fbdsschaun@inf.ufpel.edu.br>
2022-03-05 12:43:34 -0400

    fix: fix test

--- tests/cvt.cpp
+++ tests/cvt.cpp
@@ -5,6 +5,6 @@
 int main()
 {
     assert(5.8f + pinguim::cvt::to<int> * 5.99 == 10.8f);
-    assert(5.8f * pinguim::cvt::to<int> + 5.99 == 10.99f);
+    assert(5.8f * pinguim::cvt::to<int> + 5.99 == 10.99);
     return 0;
 }
