c7b0e2430813f462347983340b7043bcfdf2a2a8
fbdss <fbdsschaun@inf.ufpel.edu.br>
2022-09-02 16:44:45 -0300

    feat(opencv_wrap): implement module dependency-chain resolution

--- configure.sh
+++ configure.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-meson setup builddir "$@" -Dopencv:print_conf=true -Dopencv:shush=false
+meson setup builddir "$@" -Dopencv:print_conf=true -Dopencv:shush=false -Dopencv:modules=core,imgproc,videoio
